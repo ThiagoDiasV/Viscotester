c1655318d54c71fcf813e9061a0932f6c5b578f8
unknown <thiago76ers@gmail.com>
2019-02-14 11:59:02 -0300

    test

--- visc/rascunhos.py
+++ visc/rascunhos.py
@@ -1,7 +1,13 @@
-import re
-regex = re.compile(r'[NnSs]')
-op = ''
-while not regex.search(op):
-    op = str(input('[S/N]: ')).strip().upper()[0]
-    print(op)
-print('fim')
+from math import log10
+
+RPM = (0.3, 0.5, 0.6, 1, 1.5, 2, 2.5, 3, 4, 5, 6, 10, 12, 20, 30, 50, 60, 100, 200)
+cP = (1000, 900, 800, 700, 600, 500, 400, 300, 200, 100, 80, 60, 40, 20)
+
+lista = tuple(log10(x) for x in RPM)
+lista2 = tuple(log10(x) for x in cP)
+print(type(lista))
+for i in lista:
+    print(i)
+print(type(lista2))
+for i in lista2:
+    print(i)
