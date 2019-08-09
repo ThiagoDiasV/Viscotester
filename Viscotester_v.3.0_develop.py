89391d939982f3721c6c4f147e387e3b4895890d
unknown <thiago76ers@gmail.com>
2019-08-09 17:07:55 -0300

    now it's possible to initialize the loop with a button

--- Viscotester_v.3.0_develop.py
+++ Viscotester_v.3.0_develop.py
@@ -14,12 +14,14 @@ from math import log10
 
 
 class PrintResults(logging.Handler):
+
     def __init__(self, text):
         super().__init__()
         self.text = text
 
     def emit(self, record):
         msg = self.format(record)
+
         def append_text():
             self.text.configure(state='normal')
             self.text.insert(END, f'{msg}\n')
@@ -29,18 +31,23 @@ class PrintResults(logging.Handler):
 
 
 class Gui(Frame):
+
     def __init__(self, master):
         super().__init__()
-        self.master = master        
+        self.master = master
         self.master.title('Viscotester')
 
-        self.grid(column=0, row=0, sticky='ew')  # tentar descobrir o motivo dessa linha
+        self.grid(column=0, row=0, sticky='ew')
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure(2, weight=1)
         self.grid_columnconfigure(3, weight=1)
 
-        self.main_title = Label(self, text='Viscotester 3.0', font=('Helvetica', 18))
+        self.main_title = Label(
+                                self,
+                                text='Viscotester 3.0',
+                                font=('Helvetica', 18)
+                                )
         self.main_title.grid(column=0, row=0, sticky='ew', columnspan=4)
 
         self.filename_text = Label(self, text='Nome do arquivo')
@@ -48,10 +55,19 @@ class Gui(Frame):
         self.filename = Entry(self, text='Nome do arquivo', width=40)
         self.filename.grid(column=0, row=2)
 
-        self.init_button = Button(self, text="Iniciar análise", width=28, command=self.initializer)
+        self.init_button = Button(
+                                  self,
+                                  text="Iniciar análise", width=28,
+                                  command=self.initialize_viscotester
+                                  )
         self.init_button.grid(column=1, row=1)
 
-        self.stop_analysis = Button(self, text="Parar análise", width=28)
+        self.stop_analysis = Button(
+                                    self,
+                                    text="Parar análise",
+                                    width=28,
+                                    command=lambda: visc.change_active_status()
+                                    )
         self.stop_analysis.grid(column=1, row=2)
 
         self.save_workbook = Button(self, text="Salvar dados", width=28)
@@ -59,7 +75,7 @@ class Gui(Frame):
 
         self.launch_workbook = Button(self, text="Abrir planilha", width=28)
         self.launch_workbook.grid(column=2, row=2)
-            
+
         st = ScrolledText.ScrolledText(self, state='disabled')
         st.configure(font='TkFixedFont')
         st.grid(column=0, row=5, sticky='w', columnspan=4)
@@ -72,63 +88,80 @@ class Gui(Frame):
         logger = logging.getLogger()
         logger.addHandler(text_handler)
 
-    
-    def initializer(self):
-        t1 = threading.Thread(target=job)
-        t1.start()
+    def initialize_viscotester(self):
+        visc = Viscotester()
+        visc.active_status = True
+        return visc
 
 
-class Viscotester(Gui):
+class Viscotester:
+
     def __init__(self):
-        super().__init__()
+        self.thread = threading.Thread(target=self.job)
+        self.thread.start()
+        self.stop_threads = threading.Event()
+        self.active_status = False
 
+    @property
+    def active_status(self):
+        return active_status
 
+    @active_status.setter
+    def active_status(self, value):
+        self.active_status = value
 
-
-def validator(serial_object):
-    if len(serial_object) == 8:
-        return True
-
-
-def values_storager(*args):
-    rpm_value, cp_value, torque_value = (args[0], args[1], args[2])
-
-    if rpm_value not in registers.keys():
-        registers[rpm_value] = [[cp_value], [torque_value]]
-    elif rpm_value in registers.keys():
-        registers[rpm_value][0].append(cp_value)
-        registers[rpm_value][1].append(torque_value)
-    return registers
-
-def job():
-    ser = serial.Serial('COM1', 9600)
-    while True:
-        serial_object = ser.readline().split()
-        if validator(serial_object):
-            rpm_value, cp_value, torque_value = (
-                                                float(serial_object[3]), 
-                                                int(serial_object[7]), 
-                                                float(serial_object[5])
-                                                )
-            values = (rpm_value, cp_value, torque_value)
-            registers = values_storager(*values)
-            prints = f'RPM: {rpm_value:.>15} /// cP: {cp_value:.>15} /// Torque: {torque_value:.>15}%'
-            print(registers)
+    def change_active_status(self):
+        if self.active_status:
+            self.active_status = False
         else:
-            prints = f'ERRO!'
-    
-        #visc = Viscotester(values)
-        logging.info(prints)
-        
+            self.active_status = True
+
+    def active_status(self, value):
+        self.active_status = value
+
+    def job(self):
+
+        def validator(serial_object):
+            if len(serial_object) == 8:
+                return True
+
+        def values_storager(*args):
+            rpm_value, cp_value, torque_value = (args[0], args[1], args[2])
+
+            if rpm_value not in registers.keys():
+                registers[rpm_value] = [[cp_value], [torque_value]]
+            elif rpm_value in registers.keys():
+                registers[rpm_value][0].append(cp_value)
+                registers[rpm_value][1].append(torque_value)
+            return registers
+
+        registers = dict()
+        ser = serial.Serial('COM1', 9600)
+
+        while True:
+            print(self.active_status)
+            if self.active_status:
+                serial_object = ser.readline().split()
+                if validator(serial_object):
+                    rpm, cp, torque = (
+                                        float(serial_object[3]),
+                                        int(serial_object[7]),
+                                        float(serial_object[5])
+                                      )
+                    values = (rpm, cp, torque)
+                    registers = values_storager(*values)
+                    prints = (f'RPM: {rpm:.>15} /// '
+                              f'cP: {cp:.>15} /// '
+                              f'Torque: {torque:.>15}%')
+                    print(registers)
+                else:
+                    prints = f'ERRO!'
+                logging.info(prints)
+
 
 
 if __name__ == '__main__':
-    registers = dict()
 
     root = Tk()
-    Gui(root)
-    
-
-    
+    gui = Gui(root)
     root.mainloop()
-    t1.join()
